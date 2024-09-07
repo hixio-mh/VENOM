0d9902ae140306775afb379ba616181b21201e57
PILOT <100343607+pilot8k@users.noreply.github.com>
2024-09-07 08:54:58 +0300

    Update System Cleaner.bat

--- Files/TW/System Cleaner.bat
+++ Files/TW/System Cleaner.bat
@@ -19,12 +19,12 @@ ECHO ---------------------------------------------------------------------------
 ECHO.
 ECHO                                                 Logged in as [92m%USERNAME%[1m
 ECHO.
-
 :System Cleaner [Takes Few Minutes]
 color 9
 ECHO Stop Windows Explorer
 powershell taskkill /F /IM explorer.exe
-timeout 2 /nobreak > nul
+timeout 5 /nobreak > nul
+explorer.exe
 CLS
 ECHO Delete temporary files
 DEL /F /S /Q /A %LocalAppData%\Microsoft\Windows\Explorer\thumbcache_*.db
@@ -39,6 +39,7 @@ DEL /F /S /Q %systemdrive%\*.old
 DEL /F /S /Q %systemdrive%\recycled\*.*
 DEL /F /S /Q %systemdrive%\$Recycle.Bin\*.*
 DEL /F /S /Q %windir%\*.bak
+IF EXIST %windir%\Prefetch\*.*
 DEL /F /S /Q %windir%\Prefetch\*.*
 DEL /F /Q %userprofile%\cookies\*.*
 DEL /F /Q %userprofile%\recent\*.*
@@ -119,9 +120,9 @@ ECHO Windows Services Management
 net stop UsoSvc
 net stop bits
 net stop dosvc
+net start wuauserv
 sc start WinDefend
 sc config WinDefend start= auto
-net start wuauserv
 net stop superfetch
 net stop sysmain
 sc config wuauserv start= auto
@@ -132,7 +133,6 @@ CLS
 ECHO Run Windows Explorer
 start "" /wait "C:\Windows\System32\cleanmgr.exe" /sagerun:50
 timeout 3 /nobreak > nul
-explorer.exe
 CLS
 ECHO Clean up private folders
 del /f /s /q "%USERPROFILE%\AppData\Local\Temp\*.*"
@@ -143,81 +143,6 @@ del /f /s /q "%userprofile%\AppData\Local\Microsoft\Windows\History\*.*"
 rd /s /q "%userprofile%\AppData\Local\Microsoft\Windows\History"
 del /f /s /q "%userprofile%\AppData\Roaming\Microsoft\Windows\Cookies\*.*"
 rd /s /q "%userprofile%\AppData\Roaming\Microsoft\Windows\Cookies"
-CLS
-ECHO Registry cleaning
-reg add "HKCU\Software\Microsoft\Input\TIPC" /v "Enabled" /t REG_DWORD /d "0" /f
-reg add "HKCU\Software\Microsoft\Personalization\Settings" /v "AcceptedPrivacyPolicy" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v "Enabled" /t REG_DWORD /d "0" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Start_TrackProgs" /t REG_DWORD /d "0" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Accessibility" /v "Enabled" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\BrowserSettings" /v "Enabled" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Credentials" /v "Enabled" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Language" /v "Enabled" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Personalization" /v "Enabled" /t REG_DWORD /d "1" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Windows" /v "Enabled" /t REG_DWORD /d "1" /f
-reg add "HKLM\SYSTEM\ControlSet001\Services\DiagTrack" /v "Start" /t REG_DWORD /d "2" /f
-reg add "HKLM\SYSTEM\ControlSet001\Services\dmwappushservice" /v "Start" /t REG_DWORD /d "3" /f
-reg add "HKLM\SYSTEM\ControlSet001\Services\wuauserv" /v "Start" /t REG_DWORD /d "3" /f
-reg add "HKLM\SOFTWARE\Microsoft\SQMClient\Windows" /v "CEIPEnable" /t REG_DWORD /d "0" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Services\7971f918-a847-4430-9279-4a52d1efe18d" /v "RegisteredWithAU" /t REG_DWORD /d "0" /f
-reg delete "HKCU\SOFTWARE\Microsoft\Siuf\Rules" /v "PeriodInNanoSeconds" /f
-reg delete "HKCU\Control Panel\International\User Profile" /v "HttpAcceptLanguageOptOut" /f
-reg delete "HKCU\Software\Microsoft\Direct3D\MostRecentApplication" /va /f
-reg delete "HKCU\Software\Microsoft\MediaPlayer\Player\RecentFileList" /va /f
-reg delete "HKCU\Software\Microsoft\MediaPlayer\Player\RecentURLList" /va /f
-reg delete "HKCU\Software\Microsoft\Siuf" /f
-reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Sensor" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\AppHost" /v "EnableWebContentEvaluation" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Paint\Recent File List" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit\Favorites" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Wordpad\Recent File List" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" /v "GlobalUserDisabled" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSyncProviderNotifications" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRULegacy" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSaveMRU" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Map Network Drive MRU" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths" /va /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\PushNotifications" /v "ToastEnabled" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync" /v "SyncPolicy" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\BrowserSettings" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Credentials" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Language" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Personalization" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\SettingSync\Groups\Windows" /f
-reg delete "HKLM\SYSTEM\ControlSet001\Control\WMI\Autologger\AutoLogger-Diagtrack-Listener" /f
-reg delete "HKLM\SYSTEM\CurrentControlSet\Control\WMI\Autologger\AutoLogger-Diagtrack-Listener" /f
-reg delete "HKLM\SYSTEM\CurrentControlSet\Services\SecurityHealthService" /f
-reg delete "HKLM\SYSTEM\CurrentControlSet\Services\Sense" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Direct3D\MostRecentApplication" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\MediaPlayer\Player\RecentFileList" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\MediaPlayer\Player\RecentURLList" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\OneDrive" /f
-reg delete "HKLM\SOFTWARE\Microsoft\PolicyManager\current\device\Browser" /f
-reg delete "HKLM\SOFTWARE\Microsoft\PolicyManager\current\device\System" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Speech_OneCore\Preferences" /v "ModelDownloadAllowed" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Regedit\Favorites" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Applets\Paint\Recent File List" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DataCollection" /v "AllowTelemetry" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DataCollection" /v "DoNotShowFeedbackNotifications" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Map Network Drive MRU" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /va /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsStore" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\Biometrics" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\MRT" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\Speech" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\WMDRM" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\CredUI" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /f
-reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "DoNotShowFeedbackNotifications" /f
 CLS
 echo.
 echo.═══════════════════════════════════════════════════════════
